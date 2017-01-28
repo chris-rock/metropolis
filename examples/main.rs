e81b6c650c3a0e95f4b08971391cbfa7647c1834
Christoph Hartmann <chris@lollyrock.com>
2017-01-28 13:49:36 +0100

    update

--- examples/main.rs
+++ examples/main.rs
@@ -1,39 +1,64 @@
-// extern crate libfritz;
-//
-// fn main() {
-//     println!("Hello, Node!");
-//     println!("Hello: {}", libfritz::fibonacci(20));
-//     println!("Hello: {:?}", libfritz::get_content("http://fritz.box/login_sid.lua"));
-//
-//     println!("Hello: {:?}", libfritz::login());
-// }
-
-
-#![feature(proc_macro)]
+#![deny(warnings)]
+extern crate futures;
+extern crate hyper;
+extern crate tokio_core;
 
 #[macro_use]
 extern crate serde_derive;
 
-extern crate serde_json;
+extern crate serde;
+extern crate serde_xml;
+
+// use std::env;
+use std::io::{self, Write};
+
+use futures::Future;
+use futures::stream::Stream;
+
+use hyper::Client;
 
 #[derive(Serialize, Deserialize, Debug)]
-struct Point {
-    x: i32,
-    y: i32,
+#[allow(non_snake_case)]
+struct SessionInfo {
+    Sid: String,
+    Challenge: String,
+    BlockTime: i32,
 }
 
 fn main() {
-    let point = Point { x: 1, y: 2 };
+    // <SID>0000000000000000</SID><Challenge>2bdbdb68</Challenge><BlockTime>0</BlockTime><Rights></Rights>
+    let serialized = "<SessionInfo><Sid>0000000000000000</Sid><Challenge>2bdbdb68</Challenge><BlockTime>0</BlockTime><rights></rights></SessionInfo>";
 
-    // Convert the Point to a JSON string.
-    let serialized = serde_json::to_string(&point).unwrap();
-
-    // Prints serialized = {"x":1,"y":2}
-    println!("serialized = {}", serialized);
-
-    // Convert the JSON string back to a Point.
-    let deserialized: Point = serde_json::from_str(&serialized).unwrap();
+    // Convert the XML string back to a Point.
+    let deserialized: SessionInfo = serde_xml::from_str(&serialized).unwrap();
 
     // Prints deserialized = Point { x: 1, y: 2 }
     println!("deserialized = {:?}", deserialized);
+
+    let url = "http://fritz.box/login_sid.lua";
+
+    let url = hyper::Url::parse(&url).unwrap();
+    if url.scheme() != "http" {
+        println!("This example only works with 'http' URLs.");
+        return;
+    }
+
+    let mut core = tokio_core::reactor::Core::new().unwrap();
+    let handle = core.handle();
+    let client = Client::new(&handle);
+
+    // future
+    let work = client.get(url).and_then(|res| {
+        println!("Response: {}", res.status());
+        println!("Headers: \n{}", res.headers());
+        res
+        res.body().for_each(|chunk| {
+            io::stdout().write_all(&chunk).map_err(From::from)
+        })
+    }).map(|_| {
+
+        println!("\n\nDone.");
+    });
+
+    core.run(work).unwrap();
 }
